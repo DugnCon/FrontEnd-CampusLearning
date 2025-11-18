6c028596c1d521802e021622ebef4f8e1ab35b26
DugnCon <damdaidung9b@gmail.com>
2025-11-19 01:47:59 +0700

    up

--- frontend/user-app/src/config.js
+++ frontend/user-app/src/config.js
@@ -1,6 +1,6 @@
 
-//export const API_URL = '/user/api';
-export const API_URL = 'http://localhost:8080';
+export const API_URL = '/user/api';
+//export const API_URL = 'http://localhost:8080';
 // Socket.IO configuration
 export const SOCKET_URL = API_URL;
 

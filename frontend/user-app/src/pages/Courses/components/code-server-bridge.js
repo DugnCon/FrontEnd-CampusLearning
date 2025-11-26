0c73e3f165a177d9803de6f6b42e4642f321fb60
DugnCon <damdaidung9b@gmail.com>
2025-11-26 18:31:09 +0700

    up

--- frontend/user-app/src/pages/Courses/components/code-server-bridge.js
+++ frontend/user-app/src/pages/Courses/components/code-server-bridge.js
@@ -1,13 +1,4 @@
-/*-----------------------------------------------------------------
-* File: code-server-bridge.js
-* Author: Quyen Nguyen Duc
-* Date: 2025-07-24
-* Description: This file is a component/module for the student application.
-* Apache 2.0 License - Copyright 2025 Quyen Nguyen Duc
------------------------------------------------------------------*/
-/**
- * Bridge between EditCode component and code-server
- */
+
 
 // Polyfill for browser if it doesn't exist
 if (typeof window !== 'undefined') {

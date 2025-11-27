34ca79fa1bc954c10f920a5c65c2b11806e649aa
DugnCon <damdaidung9b@gmail.com>
2025-11-27 22:20:04 +0700

    up

--- frontend/user-app/src/components/common/About.jsx
+++ frontend/user-app/src/components/common/About.jsx
@@ -327,14 +327,14 @@ const About = () => {
               },
               {
                 title: "Email",
-                content: ["support@CampusLearning.edu.vn", "quyen.nd19@outlook.com"],
+                content: ["support@CampusLearning.edu.vn", "Authentication@gmail.com"],
                 icon: <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
               },
               {
                 title: "Địa chỉ",
-                content: ["Tầng 15, Tòa nhà Innovation", "Quận 1, TP. Hồ Chí Minh"],
+                content: ["Hồ Gươm", "Hà Nội"],
                 icon: <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
